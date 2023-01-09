ab29b18bf52c0a47753733017c26cbb52f6e974a
Paweł Szynal <29870233+Szynal@users.noreply.github.com>
2023-01-09 19:28:41 +0100

    Create Produckt.cs

---
+++ Eshop.Domain/Models/Produckt.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eshop.Domain.Models
+{
+    public class Produckt
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public double Price { private set; get; }
+
+        public Supplier Supplier { get; set; }
+
+        public string PhotoUrl { get; set; }
+
+    }
+}
